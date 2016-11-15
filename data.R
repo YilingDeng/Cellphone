a9aaff0830a9aa040a61564921116beee3d802e7
coralseu <coralseu@163.com>
2016-11-15 14:45:15 +0800

    2

--- data.R
+++ data.R
@@ -7,113 +7,183 @@ library(fastcluster)
 
 memory.limit()
 memory.size(max = FALSE)
-# rm(NA)
 # 2015.12.27 - 2016.1.6
-# 27日361 28一362 29二363 30三364 31四365 1五休 2六休 3日休 4一 5二 6三
+# 27日361 28一362 29二363 30三364 31四365 1五休1 2六休2 3日休3 4一4 5二5 6三6
 
 location <- fread("D:/data/联通数据/初赛数据/shujudasai_1.csv")
 location <- unique(location, by = c("V2", "V1"), fromFirst = TRUE)
 setkey(location, V2, V1)
+
+# sample
 location <- location[1 : 10001, ]
 length(unique(location$V2)) # 1128
 save.image()
+
+# wide data to long data
 day <- strptime(location$V1, "%Y%m%d")
 location[, c("yday", "wday") := .(yday(day), ifelse(wday(day) == 1, 7, wday(day) - 1))]
 
-lng <- data.table(gather(location[, .(day = V1, yday, wday, imei = V2, '0' = V3, 
+lng <- data.table(na.omit(gather(location[, .(day = V1, yday, wday, imei = V2, '0' = V3, 
                                       '1' = V5, '2' = V7, '3' = V9, '4' = V11, '5' = V13, 
                                       '6' = V15, '7' = V17, '8' = V19, '9' = V21, 
                                       '10' = V23, '11' = V25, '12' = V27, '13' = V29, 
                                       '14' = V31, '15' = V33, '16' = V35, '17' = V37, 
                                       '18' = V39, '19' = V41, '20' = V43, '21' = V45, 
-                                      '22' = V47, '23' = V49)], "hour", "lng", 5 : 28))
-lat <- data.table(gather(location[, .(day = V1, yday, wday, imei = V2, '0' = V4, 
+                                      '22' = V47, '23' = V49)], "hour", "lng", 5 : 28)))
+lat <- data.table(na.omit(gather(location[, .(day = V1, yday, wday, imei = V2, '0' = V4, 
                                       '1' = V6, '2' = V8, '3' = V10, '4' = V12, '5' = V14, 
                                       '6' = V16, '7' = V18, '8' = V20, '9' = V22, 
                                       '10' = V24, '11' = V26, '12' = V28, '13' = V30, 
                                       '14' = V32, '15' = V34, '16' = V36, '17' = V38, 
                                       '18' = V40, '19' = V42, '20' = V44, '21' = V46, 
-                                      '22' = V48, '23' = V50)], "hour", "lat", 5 : 28))
+                                      '22' = V48, '23' = V50)], "hour", "lat", 5 : 28)))
 location <- merge(lng, lat, by = c("imei", "day", "yday", "wday", "hour"))
 location[, hour := as.integer(hour)]
 setkey(location, imei, day, hour)
 rm(lat, lng, day)
 
-# 样本
-ggplot(data = na.omit(location[imei == "00004822f78c4bd256cefccc4b82832f" & day == "20151228", ]), aes(x = lng, y = lat)) + 
-    geom_point() + geom_path() + geom_text(aes(label = hour), size = 4)
-location[imei == "00004822f78c4bd256cefccc4b82832f" & day == "20151228", ]
-unique(location[imei == "00004822f78c4bd256cefccc4b82832f" & day == "20151228", .(lng, lat)])
-
 # coords
-coords <- na.omit(unique(location[, .(lng, lat)]))
+coords <- unique(location[, .(lng, lat)])
 coords[, c("x", "y") := .(lng, lat)]
 coordinates(coords) <- c("x", "y")
 proj4string(coords) <- CRS("+init=epsg:4326")
 coords <- spTransform(coords, CRS("+init=epsg:2335"))
 coords <- data.table(coordinates(coords), coords@data)
-ggplot(data = coords, aes(x = lng, y = lat)) + geom_point()
-ggplot(data = coords, aes(x = x, y = y)) + geom_point()
-location <- merge(location, coords, by = c("lng", "lat"), all.x = TRUE)
+location <- merge(location, coords, by = c("lng", "lat"))
 setkey(location, imei, day, hour)
+
+# ggplot(data = coords, aes(x = lng, y = lat)) + geom_point()
+# ggplot(data = coords, aes(x = x, y = y)) + geom_point()
 
 # clust
 getClust <- function(Coord) {
     Coord <- data.frame(x = Coord[, x], y = Coord[, y])
-    if(nrow(Coord) < 2) return(rep(NULL, nrow(Coord)))
+    if(nrow(Coord) == 1) return(1)
     Clust <- hclust(dist(Coord), method = "complete")
     Clust <- cutree(Clust, h = 300)
     return(Clust)
 }
 
-locationNA <- na.omit(location) ###
-locationNA[, count := .N, by = imei] ###
-locationNA <- locationNA[count >= 22, ]
 locationNew <- data.table()
-for (i in unique(locationNA$imei)) {
-    locationOne <- locationNA[imei == i, ]
+for (i in unique(location$imei)) {
+    locationOne <- location[imei == i, ]
     locationClust <- data.table(Clust = getClust(locationOne))
     locationOne <- cbind(locationOne, locationClust)
     locationNew <- rbind(locationNew, locationOne)
 }
-
-rm(i, Clust, locationOne, locationClust)
+rm(i, locationOne, locationClust, location)
 
 locationNew[, c("x", "y") := .(mean(x), mean(y)), by = .(imei, Clust)]
 setkey(locationNew, imei, day, hour)
 
-# 去除多余的
-locationNew[, ClustLag := shift(Clust, type = "lag"), by = imei]
-locationNew[, ClustDiff := .(ifelse(Clust == ClustLag, 0, 1))]
-locationNew[is.na(ClustDiff), ClustDiff := 0]
-locationNew[, ClustIndex := cumsum(ClustDiff), by = imei]
-locationNew <- unique(locationNew, by = c("imei", "ClustIndex"), fromFirst = TRUE)
-
-
-ggplot(data = na.omit(locationNew[imei == "00004822f78c4bd256cefccc4b82832f" & day == "20151228", ]), aes(x = lng, y = lat)) + 
-    geom_point() + geom_path() + geom_text(aes(label = hour), size = 4)
-
 # home
-home <- na.omit(location[(yday %in% c(362, 363, 364, 365, 4, 5, 6) & hour %in% c(21, 22, 23, 0, 1, 2, 3, 4, 5, 6)) | yday %in% c(361, 1, 2, 3), ])
-home <- home[, .(count = .N), by = .(imei, lng, lat)]
-setkey(home, imei, count)
+home <- locationNew[(yday %in% c(362, 363, 364, 365, 4, 5, 6) & hour %in% c(21, 22, 23, 0, 1, 2, 3, 4, 5, 6)) | yday %in% c(361, 1, 2, 3), ]
+home <- home[, .(home = .N), by = .(imei, Clust, x, y)]
+setkey(home, imei, home)
 home <- unique(home, by = "imei", fromLast = TRUE)
+table(home$home)
 
 # work
-work <- na.omit(location[(yday %in% c(362, 363, 364, 365, 4, 5, 6) & hour %in% c(8, 9, 10, 14, 15, 16)), ])
-work <- work[, .(count = .N), by = .(imei, lng, lat)]
-setkey(work, imei, count)
+work <- locationNew[(yday %in% c(362, 363, 364, 365, 4, 5, 6) & hour %in% c(8, 9, 10, 14, 15, 16)), ]
+work <- work[, .(work = .N), by = .(imei, Clust, x, y)]
+work <- merge(work, home[, .(imei, Clust, home)], by = c("imei", "Clust"), all.x = TRUE)
+work <- work[is.na(home), ]
+work[, home := NULL]
+setkey(work, imei, work)
 work <- unique(work, by = "imei", fromLast = TRUE)
+table(work$work)
+
+# attach home & work
+locationNew <- merge(locationNew, home[, .(imei, Clust, home)], by = c("imei", "Clust"), all.x = TRUE)
+locationNew <- merge(locationNew, work[, .(imei, Clust, work)], by = c("imei", "Clust"), all.x = TRUE)
 
 # OD
-od <- rbind(cbind(home, type = "home"), cbind(work, type = "work"))
-ggplot(data = od, aes(x = lng, y = lat, group = type)) + geom_point(aes(color = type))
+od <- locationNew[!is.na(home) | !is.na(work), .(imei, x, y, home, work)]
+od <- unique(od)
+od[, type := ifelse(is.na(work), "home", "work")]
+ggplot(data = od, aes(x = x, y = y, group = type)) + geom_point(aes(color = type)) + geom_path(alpha = 0.2)
+
+# insert start home
+homeStart <- data.table(lng = rep(NA, 11), lat = rep(NA, 11), day = c("20151227", "20151228","20151229","20151230","20151231","20160101","20160102","20160103","20160104","20160105","20160106"), 
+                      yday = c(361, 362, 363, 364, 365, 1, 2, 3, 4, 5, 6), wday = c(7, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3), hour = rep(3, 11), work = rep(NA, 11))
+homeRep <- home[rep(seq(.N), 11), ]
+setkey(homeRep, imei)
+homeStart <- cbind(homeRep, homeStart)
+withHome <- home[, imei] # remove imei which can not detect home
+locationNew <- rbind(locationNew[imei %in% withHome & hour != 3, ], homeStart)
+rm(homeStart, homeRep)
+
+# create model day
+setkey(locationNew, imei, day, hour)
+locationNew[, modelDay := ifelse(hour %in% c(0, 1, 2), yday - 1, yday)]
+locationNew[modelDay == 0, modelDay := 365]
+locationNew <- locationNew[modelDay != 360, ] # yday 6 特别少
+
+# remove duplicate location 1/3
+setkey(locationNew, imei, day, hour)
+locationNew[, ClustLag := shift(Clust, type = "lag"), by = .(imei, modelDay)]
+locationNew[, ClustDiff := .(ifelse(Clust == ClustLag, 0, 1))]
+locationNew[is.na(ClustDiff), ClustDiff := 0]
+locationNew[, ClustIndex := cumsum(ClustDiff), by = .(imei, modelDay)]
+locationNew <- unique(locationNew, by = c("imei", "modelDay", "ClustIndex"), fromFirst = TRUE)
+
+# filter data 考虑是否在insert start home之前加入
+locationNew[, count := .N, by = .(imei, modelDay)]
+dayCount <- unique(locationNew[, .(imei, modelDay, count)])
+table(dayCount$count)
+locationNew <- locationNew[count > 1, ]
+
+# generate tour 考虑删除只有1个activty的tour，即晚上回家
+locationNew[, TourIndex := ifelse(is.na(home), 0, 1), by = .(imei, modelDay)]
+locationNew[, tour := cumsum(TourIndex), by = .(imei, modelDay)]
+
+# generate activtiy & motif
+getActivity <- function(Clust) {
+  Activity <- vector()
+  Activity[1] <- 1
+  j <- 1
+  if (length(Clust) == 1) {
+    return(Activity)
+  }
+  else {
+    for (i in 2 : length(Clust)) {
+      if (Clust[i] %in% Clust[1 : i - 1]) {
+        Activity[i] <- Activity[match(Clust[i], Clust[1 : i - 1])]
+        
+      }
+      else {
+        j <- j + 1
+        Activity[i] <- j
+      }
+    }
+    return(Activity)
+  }
+}
+
+setkey(locationNew, imei, day, hour)
+locationNew[, TourDiff := cumsum(TourIndex)]
+for (i in 1 : max(locationNew[, TourDiff])) {
+  Activity <- getActivity(locationNew[TourDiff == i, Clust])
+  locationNew[TourDiff == i, activity := Activity]
+  locationNew[TourDiff == i & TourIndex == 1, pattern := paste(Activity, collapse = '-')]
+}
+rm(i, Activity)
+locationNew <- locationNew[is.na(pattern) | pattern != "1", ]
+# locationNew[, nrow := 1 : nrow(locationNew)]
+# View(locationNew[, .(imei, day, hour, Clust, yday, modelDay, home, work, TourIndex, tour, activity, pattern, nrow)])
+
+# view motif
+sort(table(locationNew[, pattern]))
+length(locationNew[!is.na(pattern), pattern]) # 11677
+length(unique(locationNew[!is.na(pattern), pattern])) # 307
 
 
 
-
-
-
+# 样本 "20151228" 不能完全排除中途点
+ggplot(data = locationNew[imei == "00004822f78c4bd256cefccc4b82832f" & modelDay == 362, ], aes(x = x, y = y)) + 
+  geom_point() + geom_path() + geom_text(aes(label = hour), size = 4)
+locationNew[imei == "00004822f78c4bd256cefccc4b82832f" & modelDay == 362, ]
+unique(locationNew[imei == "00004822f78c4bd256cefccc4b82832f" & day == 362, .(x, y)])
 
 
 
